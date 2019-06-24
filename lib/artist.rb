daa69850f58fe07ee8a3af20b3eb856364d461ca
Lukie Kang <lukiek+f@gmail.com>
2019-06-24 21:09:22 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,41 @@
+class Artist
+attr_accessor :name, :all, :find_or_create_by_name
+@@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def songs
+    @songs
+  end
+
+  def save
+    @@all << self
+  end # of save
+
+  def self.all
+    @@all
+  end
+
+  def self.find_or_create_by_name(name)
+    puts @@all
+    if @@all.find{ |artist| artist.name == name}
+      return
+    else
+      new_artist = self.new(name)
+      @@all << new_artist
+      new_artist
+    end # conditional
+  end # find or create.
+
+  def print_songs
+    @songs.each { |song| puts song.name}
+  end
+
+end # of class
---
+++ lib/song.rb
@@ -0,0 +1,26 @@
+class Song
+attr_accessor :name, :new_by_filename, :artist
+
+
+  def initialize(name)
+    @name = name
+  end
+
+
+
+  def self.new_by_filename(filename)
+    split_name = filename.split("-")
+    song_name = split_name[1].strip
+    artist_name = split_name[0].strip
+    puts "songname#{song_name}X#{artist_name}"
+    new_Song = Song.new(song_name)
+    new_Song.artist = Artist.find_or_create_by_name(artist_name)
+    # new_Song.name = sog
+    # puts new_Song.name
+
+
+    new_Song
+  end
+
+
+end # of class
